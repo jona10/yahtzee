8d7d321d5915ce26523b0762962832a0a702fab2
Jonathan Falardeau <jonathan.falardeau@gmail.com>
2017-08-27 13:31:23 -0400

    Generalize the working of both score card sections.

---
+++ src/Yahtzee.Core/ScoreCardSection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yahtzee.Core
+{
+    public abstract class ScoreCardSection
+    {
+        protected ScoreCardSection()
+        {
+            Items = new Dictionary<ScoreCard.Items, Score>();
+        }
+
+        protected Dictionary<ScoreCard.Items, Score> Items { get; }
+
+        public void Preview(IEnumerable<int> dice)
+        {
+            foreach (var current in Items.Values)
+                current.Preview(dice);
+        }
+
+        public void Lock(ScoreCard.Items item)
+        {
+            if (!Items.ContainsKey(item))
+                throw new Exception(item + " is not part of this section.");
+
+            Items[item].LockIn();
+            Items.Values.ToList().ForEach(x => x.Reset());
+        }
+    }
+}
--- src/Yahtzee.Core/UpperSection.cs
+++ src/Yahtzee.Core/UpperSection.cs
@@ -4,58 +4,30 @@ using System.Linq;
 
 namespace Yahtzee.Core
 {
-    public class UpperSection
+    public class UpperSection : ScoreCardSection
     {
         private const int UpperBonusLimit = 63;
         private const int UpperBonusValue = 35;
 
-        private readonly Dictionary<ScoreCard.Items, Score> _items;
-
         public UpperSection(Func<IEnumerable<int>, int> onesScorer, Func<IEnumerable<int>, int> twosScorer, Func<IEnumerable<int>, int> threesScorer, Func<IEnumerable<int>, int> foursScorer, Func<IEnumerable<int>, int> fivesScorer, Func<IEnumerable<int>, int> sixesScorer)
         {
-            _items = new Dictionary<ScoreCard.Items, Score>();
-            _items.Add(ScoreCard.Items.Ones, new Score(onesScorer));
-            _items.Add(ScoreCard.Items.Twos, new Score(twosScorer));
-            _items.Add(ScoreCard.Items.Threes, new Score(threesScorer));
-            _items.Add(ScoreCard.Items.Fours, new Score(foursScorer));
-            _items.Add(ScoreCard.Items.Fives, new Score(fivesScorer));
-            _items.Add(ScoreCard.Items.Sixes, new Score(sixesScorer));
+            Items.Add(ScoreCard.Items.Ones, new Score(onesScorer));
+            Items.Add(ScoreCard.Items.Twos, new Score(twosScorer));
+            Items.Add(ScoreCard.Items.Threes, new Score(threesScorer));
+            Items.Add(ScoreCard.Items.Fours, new Score(foursScorer));
+            Items.Add(ScoreCard.Items.Fives, new Score(fivesScorer));
+            Items.Add(ScoreCard.Items.Sixes, new Score(sixesScorer));
         }
 
-        private int SumOfLockedScore => _items.Values.Where(x => x.IsLocked()).Select(x => x.Value).Sum();
+        private int SumOfLockedScore => Items.Values.Where(x => x.IsLocked()).Select(x => x.Value).Sum();
 
-        public int Ones => _items[ScoreCard.Items.Ones].Value;
-        public int Twos => _items[ScoreCard.Items.Twos].Value;
-        public int Threes => _items[ScoreCard.Items.Threes].Value;
-        public int Fours => _items[ScoreCard.Items.Fours].Value;
-        public int Fives => _items[ScoreCard.Items.Fives].Value;
-        public int Sixes => _items[ScoreCard.Items.Sixes].Value;
+        public int Ones => Items[ScoreCard.Items.Ones].Value;
+        public int Twos => Items[ScoreCard.Items.Twos].Value;
+        public int Threes => Items[ScoreCard.Items.Threes].Value;
+        public int Fours => Items[ScoreCard.Items.Fours].Value;
+        public int Fives => Items[ScoreCard.Items.Fives].Value;
+        public int Sixes => Items[ScoreCard.Items.Sixes].Value;
         public int Score => SumOfLockedScore + Bonus;
         public int Bonus => SumOfLockedScore >= UpperBonusLimit ? UpperBonusValue : 0;
-
-        public void Lock(ScoreCard.Items item)
-        {
-            switch (item)
-            {
-                case ScoreCard.Items.Ones:
-                case ScoreCard.Items.Twos:
-                case ScoreCard.Items.Threes:
-                case ScoreCard.Items.Fours:
-                case ScoreCard.Items.Fives:
-                case ScoreCard.Items.Sixes:
-                    _items[item].LockIn();
-                    _items.Values.ToList().ForEach(x => x.Reset());
-                    break;
-
-                default:
-                    throw new Exception(item + " is not part of the upper section.");
-            }
-        }
-
-        public void Preview(IEnumerable<int> dice)
-        {
-            foreach (var current in _items.Values)
-                current.Preview(dice);
-        }
     }
 }
